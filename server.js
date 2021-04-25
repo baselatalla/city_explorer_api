e46238b51b70f077e07a84d19ef1860aeeea12bf
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-04-25 17:56:21 +0300

    done lab-06

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+
+require('dotenv').config();
+
+const cors = require('cors');
+
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+server.use(cors());
+
+server.get('/', (req,res)=>{
+  res.send('سلاااام على الزلااام ');
+});
+
+server.get('/location',(req,res)=>{
+  //fetch the data that inside locaion.json file
+  let locationData = require('./data/location.json');
+  let locationRes = new Location(locationData);
+  console.log(locationRes);
+  res.send(locationRes);
+});
+
+server.get('/weather',(req,res)=>{
+  //fetch the data that inside locaion.json file
+  let weatherData = require('./data/weather.json');
+
+  weatherData.data.forEach(element => {
+    new Weather(element.weather['description'],element.valid_date);
+  });
+  res.send(weatherArray);
+});
+
+let weatherArray = [];
+
+function Location(locationData) {
+  this.search_query = 'Lynwood';
+  this.formatted_query = locationData[0].display_name;
+  this.latitude = locationData[0].lat;
+  this.longitude = locationData[0].lon;
+}
+function Weather(forecast,time){
+  this.forecast = forecast;
+  this.time = time;
+  weatherArray.push(this);
+}
+
+server.listen(PORT,()=>{
+  console.log(`listening on port ${PORT}`);
+});
